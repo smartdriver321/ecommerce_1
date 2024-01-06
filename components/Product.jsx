6474fc56db89afe85e428b79d6faefa548456f1f
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-01-07 06:37:59 +0700

    Display products on the home page

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,5 +1,23 @@
+import { urlFor } from '@/lib/client';
+import Link from 'next/link';
 import React from 'react';
 
-export default function Product() {
-  return <div>Product</div>;
+export default function Product({ product: { image, name, slug, price } }) {
+  return (
+    <div>
+      <Link href={`/product/${slug.current}`}>
+        <div className='product-card'>
+          <img
+            alt='product'
+            src={urlFor(image && image[0])}
+            width={250}
+            height={250}
+            className='product-image'
+          />
+          <p className='product-name'>{name}</p>
+          <p className='product-price'>${price}</p>
+        </div>
+      </Link>
+    </div>
+  );
 }
