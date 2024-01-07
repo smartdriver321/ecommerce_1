acf03c4fd622834e21a063ebedf3fab71dfa72ea
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-01-07 07:28:42 +0700

    Add Layout and FooterBanner component on the homepage

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,5 +1,21 @@
+import Head from 'next/head';
 import React from 'react';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
-export default function Layout() {
-  return <div>Layout</div>;
+export default function Layout({ children }) {
+  return (
+    <div className='layout'>
+      <Head>
+        <title>JS Mastery Store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className='main-container'>{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 }
